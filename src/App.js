c916f279f0e9070596db17f3a930c29998d3f30f
RosuAdrianMihai <rosumihai16@gmail.com>
2022-06-03 10:58:50 +0300

    final-version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import { React, useState } from "react";
-import "./index.css"
+import "./index.css";
 import Navbar from "./components/Navbar";
 import BlackModal from "./components/BlackModal";
 import ContentContainer from "./components/ContentContainer";
 import ImageSlider from "./components/ImageSlider";
 
-function App(){
-     const [menu, setMenu] = useState(false);
-     const [itemsInCart, setItemsInCart] = useState(0);
-     const [image, setImage] = useState(1);
-     const [imageSlide, setImageSlide] = useState(false);
-     const [copyImage, setCopyImage] = useState(image);
+function App() {
+  const [menu, setMenu] = useState(false);
+  const [itemsInCart, setItemsInCart] = useState(0);
+  const [image, setImage] = useState(1);
+  const [imageSlide, setImageSlide] = useState(false);
+  const [copyImage, setCopyImage] = useState(image);
 
-    return(
-        <div>
-            <BlackModal menuState={menu} setMenuStatus={setMenu} imageSlide={imageSlide} />
-            <Navbar setMenuStatus={setMenu} menuState={menu} itemsInCart={itemsInCart} setItemsInCart={setItemsInCart} imageSlide={imageSlide} />
-            <ContentContainer menuState={menu} itemsInCart={itemsInCart} setItemsInCart={setItemsInCart} image={image} setImage={setImage} imageSlide={imageSlide} setImageSlide={setImageSlide} />
-            <ImageSlider copyImage={copyImage} setCopyImage={setCopyImage} imageSlide={imageSlide} setImageSlide={setImageSlide}/>
-        </div>
-    )
+  return (
+    <div>
+      <BlackModal
+        menuState={menu}
+        setMenuStatus={setMenu}
+        imageSlide={imageSlide}
+      />
+      <Navbar
+        setMenuStatus={setMenu}
+        menuState={menu}
+        itemsInCart={itemsInCart}
+        setItemsInCart={setItemsInCart}
+        imageSlide={imageSlide}
+      />
+      <ContentContainer
+        menuState={menu}
+        itemsInCart={itemsInCart}
+        setItemsInCart={setItemsInCart}
+        image={image}
+        setImage={setImage}
+        imageSlide={imageSlide}
+        setImageSlide={setImageSlide}
+      />
+      <ImageSlider
+        copyImage={copyImage}
+        setCopyImage={setCopyImage}
+        imageSlide={imageSlide}
+        setImageSlide={setImageSlide}
+      />
+    </div>
+  );
 }
 
 export default App;
